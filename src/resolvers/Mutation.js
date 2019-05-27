028c3f9cb79278543ef44b0bef056042601ca6ad
1337erik <erikpwhite@gmail.com>
2019-05-27 16:31:20 -0400

    finished subscriptions section

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -40,6 +40,28 @@ async function login( parent, args, context, info ) {
     }
 }
 
+async function vote( parent, args, context, info ) {
+
+    const userId = getUserId( context );
+
+    const linkExists = await context.prisma.$exists.vote({
+
+        user: { id: userId },
+        link: { id: args.linkId }
+    })
+
+    if( linkExists ) {
+
+        throw new Error( `Already voted for link: ${args.linkId}` );
+    }
+
+    return context.prisma.createVote({
+
+        user: { connect: { id: userId } },
+        link: { connect: { id: args.linkId } }
+    })
+}
+
 async function post( root, args, context ) {
 
     const userId = getUserId( context );
@@ -77,5 +99,6 @@ module.exports = {
 
     signup,
     login,
-    post
+    post,
+    vote
 }
---
+++ src/resolvers/Vote.js
@@ -0,0 +1,15 @@
+function link( parent, args, context ) {
+
+    return context.prisma.vote({ id: parent.id }).link()
+}
+
+function user( parent, args, context ) {
+
+    return context.prisma.vote({ id: parent.id }).user()
+}
+
+module.exports = {
+
+    link,
+    user
+}
